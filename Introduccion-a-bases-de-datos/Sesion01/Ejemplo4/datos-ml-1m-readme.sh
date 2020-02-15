e9392b8e4052d6982d18e347b07d0d2338cfc7d8
david94 <c.d._home@hotmail.com>
2020-02-15 15:13:31 -0600

    Postwork

---
+++ Introduccion-a-bases-de-datos/Sesion01/Ejemplo4/datos-ml-1m-readme.sh
@@ -0,0 +1,7 @@
+mkdir Datos #Se crea directorio
+cd Datos #Nos cambiamos de directorio
+# Se descarga archivo de texto
+curl -O http://files.grouplens.org/datasets/movielens/ml-1m-README.txt
+ls #se listan los archivos
+
+
