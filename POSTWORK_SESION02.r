863cdb89012ac29023b792c368f48b907f125c41
2013361048 <60289987+2013361048@users.noreply.github.com>
2020-03-11 23:10:38 -0600

    Postwork_Sesion02_Rstudio

---
+++ POSTWORK_SESION02.r
@@ -0,0 +1,10 @@
+nrow(auto.mpg1)
+ncol(auto.mpg1)
+dim(auto.mpg1)
+names(auto.mpg1)
+
+print(paste('El número de columnas de nuestro dataframe es:', ncol(auto.mpg1)))
+print(paste('El número de renglones de nuesrtro dataframe es:', nrow(auto.mpg1)))
+
+str(auto.mpg1)
+summary(auto.mpg1)
