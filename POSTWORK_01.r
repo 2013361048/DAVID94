ae891e51f42611f4d61c7f3c270145d98d58aa6d
2013361048 <60289987+2013361048@users.noreply.github.com>
2020-03-11 23:09:26 -0600

    Postwork_Sesion_01_Restudio

---
+++ POSTWORK_01.r
@@ -0,0 +1,20 @@
+#POSTWORKSESION 01
+# Suma
+5 + 5 
+
+# Resta
+5 - 5 
+
+# Multiplicación
+3 * 5
+
+# División
+(5 + 5)/2 
+
+# Potencia
+5^2
+# Módulo o residuo
+5%%3
+ls("package:ggplot2")
+
+?ggplot2
